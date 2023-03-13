706debefae4e201aa8796f824b5b24c61c0fc18e
phukon <rikiphukon16@gmail.com>
2023-03-13 13:08:35 +0530

    app

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,30 @@
+import {Route, Routes} from 'react-router-dom';
+import Layout from './Layout';
+import { IndexPage, LoginPage, ResourcePage, EventsPage, CommunityPage, RegisterPage, Contributors, Contact, Contribute } from "./components/Index";
+
+import { UserContextProvider } from './UserContext';
+import './App.css';
+
+
+
+
 function App() {
   return (
-    <h1 className="text-1xl font-bold underline">
-      Hello world!
-    </h1>
+    <UserContextProvider>
+         <Routes>
+          <Route path='/' element={<Layout/>}>
+              <Route index element={<IndexPage/>}/>
+              <Route path='/login' element = {<LoginPage/>}/>
+              <Route path='/register' element = {<RegisterPage/>}/>
+              <Route path='/resources' element = {<ResourcePage/>}/>
+              <Route path='/events' element = {<EventsPage/>}/>
+              <Route path='/community' element = {<CommunityPage/>}/>
+              <Route path='/contact' element = {<Contact/>}/>
+              <Route path='/contributors' element = {<Contributors/>}/>
+              <Route path='/contribute' element = {<Contribute/>}/>
+          </Route>
+        </Routes>
+    </UserContextProvider>
   );
 }
 
